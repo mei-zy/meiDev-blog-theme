b91e23b42150a2bbb2f709f8605d9bd8c7469a74
urther <alswlkku@ajou.ac.kr>
2022-07-25 12:29:30 +0900

    feat : Add layout components for Head markup
    - add layout components for Head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import type { AppProps } from "next/app";
+import Layout from "./components/layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  // eslint-disable-next-line react/no-children-prop
+  return <Layout children={<Component {...pageProps} />}></Layout>;
 }
 
 export default MyApp;
