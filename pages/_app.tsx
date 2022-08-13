4a56b2978254c901247ea1cabb352b5e4b2ec2af
urther <alswlkku@ajou.ac.kr>
2022-08-13 19:39:33 +0900

    remove : remove unused file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,15 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { useState } from "react";
-import { ThemeProvider } from "styled-components";
 import GlobalStyles from "../styles/globalStyles";
-import { darkTheme, lightTheme } from "../styles/theme";
-import Header from "../components/header";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [theme, setTheme] = useState("light");
-
   return (
     <>
       <Head>
         <meta charSet="utf-8" />
       </Head>
-      <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
-        <GlobalStyles />
-        <Header theme={theme} setTheme={setTheme} />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <GlobalStyles />
+      <Component {...pageProps} />
     </>
   );
 }
