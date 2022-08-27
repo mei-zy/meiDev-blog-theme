4f739438fba25fb3b1204514b0ab7c6cc46b1a98
urther <alswlkku@ajou.ac.kr>
2022-08-27 20:15:31 +0900

    chore : Edit contentlayer setting
    - edit contentlayer setting (add tags)
    - change ts strict mode

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
 const { withContentlayer } = require("next-contentlayer");
-/** @type {import('next').NextConfig} */
+
 const nextConfig = withContentlayer({
   reactStrictMode: true,
-  swcMinify: true,
-  webpack: (config) => {
-    // 아래를 추가합니다.
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
-  },
 });
 
 module.exports = nextConfig;
